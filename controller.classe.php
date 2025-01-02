453c8e102ec7cd780007b897cdba1c73436d5a20
KAWTARDAKHAM <114149120+KAWTARDAKHAM@users.noreply.github.com>
2025-01-03 00:37:16 +0100

    vue de login et ajout des infos a la base de donnes
    Login and sign up vue

--- controller.classe.php
+++ controller.classe.php
@@ -8,6 +8,7 @@ class Controller
         $this->m = new Model();
     }
 
+
     #l'action qui renvoie vers la page home
     public function homeAction()
     {
@@ -17,9 +18,59 @@ class Controller
     public function showAllEtudiantActiuon()
     {
         $etudiants = $this->m->getAllEtudiants();
-        $content = 'views/showAllEtudiant.view.php';
+        include 'views/showAllEtudiant.view.php';
         include 'views/base.view.php';
+    }   
+
+    public function AddUser(){
+        $profil=array($_POST['email'],$_POST['pswd'],$_POST['statut']);
+        $prof=array($_POST['email'],$_POST['nom'],$_POST['prenom'],$_POST['departement']);
+        $admin=array($_POST['email'],$_POST['nom'],$_POST['prenom']);
+        $student=array($_POST['email'],$_POST['nom'],$_POST['prenom'],$_POST['departement'],$_POST['filiere'],$_POST['classe']);
+
+        $this->m->AddUser($profil);
+
+        $statut=$_POST['statut'];
+
+        switch ($statut) {
+            case 'ADMIN':
+                $this->m->AddAdmin($admin);
+                echo "Administrateur ajouté avec succès.";
+                break;
+            case 'PROF':
+                $this->m->AddProfessor($prof);
+                echo "Professeur ajouté avec succes";
+                break;
+
+            case 'STUD':
+                $this->m->AddStudent($student);
+                $message = "Étudiant ajouté avec succès.";
+                break;
     }
+            header('location:controller.class.php');
+}
+
+
+    public function showProfiles(){
+
+        $profil=array($_POST['email'],$_POST['pswd']);
+        $log=$_POST['email'];
+        $pass=$_POST['pswd'];
+        $stat=$this->m->Statut($log,$pass);
+        switch($stat){
+
+            /*On va faire une redirection vers une vue d'affichage selon chaque profil*/
+            case 'ADMIN':
+                /**/              
+            case 'STUD':
+                /**/
+            case 'PROF':
+                 /**/
+        }
+    }
+
+
+
     #La fonction qui controlle toutes les actions
     public function action()
     {
@@ -31,6 +82,8 @@ class Controller
         switch( $action )
         {
             case 'home': $this->homeAction(); break;
+            case 'Addprofil':$this->AddUser();break;
+            case 'show':$this->showProfiles();break;
             case 'showAllEtudiants': $this->showAllEtudiantActiuon(); break;
         }
     }
--- model.classe.php
+++ model.classe.php
@@ -8,13 +8,43 @@ class Model
         define("PASS","");
         $this->db = new PDO("mysql:host=localhost; dbname=univmanager", USER, PASS);
     }
-
+    
     public function getAllEtudiants()
     {
         $query = "select * from etudiant";
         $prepQuery = $this->db->prepare($query);
         $prepQuery->execute();
         return $prepQuery->fetchAll(PDO::FETCH_ASSOC);
+    }
+
+    /* ajouter fonction d'ajout d'un profil */
+    public function AddUser($user){
+        $query=$this->db->prepare("Insert into utilisateur values(?,?,?)");
+        $query->execute($user);
+    }
+    public function AddAdmin($user){
+        $query=$this->db->prepare("insert into admin values(?,?,?)");
+        $query->execute($user);
+    }
+
+    public function AddStudent($user){
+        $query=$this->db->prepare("insert into etudiant values(?,?,?,?,?,?)");
+        $query->execute($user);
+    }
+    public function AddProfessor($user){
+        $query=$this->db->prepare("insert into professeur values(?,?,?,?) ");
+        $query->execute($user);
+    }
+    public function Statut($log,$pass):string{
+
+        $query = $this->db->prepare("SELECT type FROM utilisateur WHERE login = :login AND password = :password");
+        $query->bindParam(':login', $log, PDO::PARAM_STR);
+        $query->bindParam(':password', $pass, PDO::PARAM_STR);
+        $query->execute();
+        $type = $query->fetch(PDO::FETCH_ASSOC);
+        $statut = $query->fetchColumn();
+
+        return $statut;
     }
 }
 ?>
